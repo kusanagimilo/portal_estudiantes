57592a1724ceaa7253e45b9ee22ce1493ba76c24
Juan Camilo Cruz Franco <kusanagimilo@gmail.com>
2017-04-28 17:26:54 -0500

    prueba js
    prueba js

---
+++ utilidades.js
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function ColocarBotonCotizacion() {
+
+    var paso;
+    for (paso = 0; paso < 5; paso++) {
+        // Se ejecuta 5 veces, con valores desde paso desde 0 hasta 4.
+        console.log('Dando un paso al Este');
+    }
+    
+}
+
